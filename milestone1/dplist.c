3009ddfb69c62da7771a6df95e461b40c09d31f9
nur <nur.aldaonggar@student.kuleuven.be>
2022-11-02 18:38:23 +0000

    milestone not finished

--- milestone1/dplist.c
+++ milestone1/dplist.c
@@ -69,51 +69,191 @@ dplist_t *dpl_create(// callback functions
 }
 
 void dpl_free(dplist_t **list, bool free_element) {
+    if (list == NULL) {
+        return;
+    }
 
-    //TODO: add your code here
+    dplist_node_t *current = dpl_get_reference_at_index(*list, 0);
+    if (free_element) {
+        for (int i = 0;  i < dpl_size(*list); i++) {
+            (*list)->element_free(current->element);
+            current = current->next;
+        }
+    }
+    free(*list);
+    *list = NULL;
 
 }
 
 dplist_t *dpl_insert_at_index(dplist_t *list, void *element, int index, bool insert_copy) {
+    dplist_node_t *ref_at_index, *list_node;
+    if (list == NULL) return NULL;
 
-    //TODO: add your code here
+    if (insert_copy) {
+        element = list->element_copy(element);
+    }
+
+    list_node = malloc(sizeof(dplist_node_t));
+    DPLIST_ERR_HANDLER(list_node == NULL, DPLIST_MEMORY_ERROR);
+    list_node->element = element;
+    // pointer drawing breakpoint
+    if (list->head == NULL) { // covers case 1
+        list_node->prev = NULL;
+        list_node->next = NULL;
+        list->head = list_node;
+        // pointer drawing breakpoint
+    } else if (index <= 0) { // covers case 2
+        list_node->prev = NULL;
+        list_node->next = list->head;
+        list->head->prev = list_node;
+        list->head = list_node;
+        // pointer drawing breakpoint
+    } else {
+        ref_at_index = dpl_get_reference_at_index(list, index);
+        assert(ref_at_index != NULL);
+        // pointer drawing breakpoint
+        if (index < dpl_size(list)) { // covers case 4
+            list_node->prev = ref_at_index->prev;
+            list_node->next = ref_at_index;
+            ref_at_index->prev->next = list_node;
+            ref_at_index->prev = list_node;
+            // pointer drawing breakpoint
+        } else { // covers case 3
+            assert(ref_at_index->next == NULL);
+            list_node->next = NULL;
+            list_node->prev = ref_at_index;
+            ref_at_index->next = list_node;
+            // pointer drawing breakpoint
+        }
+    }
+    
+    return list;
 
 }
 
 dplist_t *dpl_remove_at_index(dplist_t *list, int index, bool free_element) {
 
-    //TODO: add your code here
+    if (list == NULL) {
+        return NULL;
+    } 
+
+    if (dpl_size(list) == 0) {
+        return list;
+    }
+    
+    if (index < 0) {
+        index = 0;
+    } else if (index >= dpl_size(list)) {
+        index = dpl_size(list) - 1;
+    }
+
+    dplist_node_t *current = list->head;
+
+    for (int i = 0; i < index; i++) {
+        current = current->next;
+    }
+
+    if (free_element) {
+        list->element_free(current->element);
+    }
+
+    if (list->head == current) {
+        list->head = current->next;
+    }
+    if (current->next != NULL) {
+        current->next->prev = current->prev;
+    }
+    if (current->prev != NULL) {
+        current->prev->next = current->next;
+    }
+
+    return list;
 
 }
 
 int dpl_size(dplist_t *list) {
 
-    //TODO: add your code here
+    if (list == NULL)
+        return -1;
+    int cnt = 0;
+    dplist_node_t * current = list->head;
+    while (current != NULL) {
+        cnt++;
+        current = current->next;
+    }
+
+    return cnt;
 
 }
 
 void *dpl_get_element_at_index(dplist_t *list, int index) {
 
-    //TODO: add your code here
+    if (list == NULL || dpl_size(list) == 0) {
+        return NULL;
+    }
+
+    dplist_node_t * current = list->head; 
+
+    if (index < 0) {
+        index = 0;
+    } else if (index >= dpl_size(list)) {
+        index = dpl_size(list) - 1;
+    }
+
+    for (int i = 0;  i < index; i++) {
+        current = current->next;
+    }
+    
+    return current->element;
 
 }
 
 int dpl_get_index_of_element(dplist_t *list, void *element) {
 
-    //TODO: add your code here
+    if (list == NULL) {
+        return -1;
+    }
+
+    dplist_node_t * current = list->head; 
+
+    for (int i = 0;  i < dpl_size(list); i++) {
+        if (!(list->element_compare(current->element, element))) {
+            return i;
+        }
+        current = current->next;
+    }
+    
+    return -1;
 
 }
 
 dplist_node_t *dpl_get_reference_at_index(dplist_t *list, int index) {
 
-    //TODO: add your code here
+    int count;
+    dplist_node_t *dummy;
+    DPLIST_ERR_HANDLER(list == NULL, DPLIST_INVALID_ERROR);
+    if (list->head == NULL) return NULL;
+    for (dummy = list->head, count = 0; dummy->next != NULL; dummy = dummy->next, count++) {
+        if (count >= index) return dummy;
+    }
+    return dummy;
 
 }
 
 void *dpl_get_element_at_reference(dplist_t *list, dplist_node_t *reference) {
 
-    //TODO: add your code here
+    dplist_node_t *current = list->head;
 
+    if (list == NULL || dpl_size(list) == 0 || reference == NULL) {
+        return NULL;
+    }
+    
+    for (int i = 0; i < dpl_size(list); i++) {
+        if (current == reference) return reference->element;
+    }
+    
+    return NULL;
+    
 }
 
 
--- milestone1/dplist_test.c
+++ milestone1/dplist_test.c
@@ -15,6 +15,9 @@ void* element_copy(void * element);
 void element_free(void ** element);
 int element_compare(void * x, void * y);
 
+my_element_t *character, *character1, *character2, *character3, *character4, *character5, *character6;
+
+
 void * element_copy(void * element) {
     my_element_t* copy = malloc(sizeof (my_element_t));
     char* new_name;
@@ -36,12 +39,41 @@ int element_compare(void * x, void * y) {
 }
 
 void setup(void) {
-    // Implement pre-test setup
+    character->id = 0;
+    character->name = "F";
+    character1->id = 1;
+    character1->name = "A";
+    character2->id = 2;
+    character2->name = "B";
+    character3->id = 3;
+    character3->name = "C";
+    character4->id = 4;
+    character4->name = "G";
+    character5->id = 5;
+    character5->name = "D";
+    character6->id = 6;
+    character6->name = "E";
+    return;
 }
 
 void teardown(void) {
-    // Implement post-test teardown
+    free(character);
+    character = NULL;
+    free(character1);
+    character1 = NULL;
+    free(character2);
+    character2 = NULL;
+    free(character3);
+    character3 = NULL;
+    free(character4);
+    character4 = NULL;
+    free(character5);
+    character5 = NULL;
+    free(character6);
+    character6 = NULL;
+    return;
 }
+
 START_TEST(test_ListFree)
     {
         // Test free NULL, don't use callback
@@ -65,15 +97,119 @@ START_TEST(test_ListFree)
         ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
 
         // TODO : Test free with one element, also test if inserted elements are set to NULL
+        list = dpl_create(element_copy, element_free, element_compare);
+        //dpl_insert_at_index(list, character, 0, false);
+        dpl_free(&list, true);
+        ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
 
         // TODO : Test free with multiple element, also test if inserted elements are set to NULL
 
     }
 END_TEST
 
-//START_TEST(test_nameOfYourTest)
-//  Add other testcases here...
-//END_TEST
+START_TEST(test_ListInsertAtIndexListNULL)
+    {
+        // Test inserting at index -1
+        dplist_t *result = dpl_insert_at_index(NULL, character, -1, false);
+        ck_assert_msg(result == NULL, "Failure: expected list to be NULL");
+        
+        // TODO : Test inserting at index 0
+        dpl_insert_at_index(result, character, 0, true);
+        ck_assert_msg(result == NULL, "Failure: expected list to be NULL");
+        
+        // TODO : Test inserting at index 99
+        dpl_insert_at_index(result, character, 99, true);
+        ck_assert_msg(result == NULL, "Failure: expected list to be NULL");
+    }
+END_TEST
+
+START_TEST(test_ListInsertAtIndexListEmpty)
+{
+    // Test inserting at index -1
+    dplist_t *list = dpl_create(element_copy, element_free, element_compare);
+    dplist_t *result = dpl_insert_at_index(list, character, -1, true);
+    ck_assert_msg(dpl_size(result) == 1, "Failure: expected list to have size of 1, got a size of %d",
+                                         dpl_size(result));
+    dpl_free(&list, true);
+    
+    // TODO : Test inserting at index 0
+    list = dpl_create(element_copy, element_free, element_compare);
+    result = dpl_insert_at_index(list, character, 0, false);
+    ck_assert_msg(dpl_size(result) == 1, "Failure: expected list to have size of 1, got a size of %d",
+                                         dpl_size(result));
+    dpl_free(&list, true);
+
+    // TODO : Test inserting at index 99
+    list = dpl_create(element_copy, element_free, element_compare);
+    result = dpl_insert_at_index(list, character, 99, false);
+    ck_assert_msg(dpl_size(result) == 1, "Failure: expected list to have size of 1, got a size of %d",
+                                         dpl_size(result));
+    dpl_free(&list, true);
+
+}
+END_TEST
+
+START_TEST(test_ListGetElementAtIndex)
+{
+    dplist_t *list = dpl_create(element_copy, element_free, element_compare);
+    dpl_insert_at_index(list, character, 0, false);
+    dpl_insert_at_index(list, character1, 1, false);
+    dpl_insert_at_index(list, character2, 2, false);
+    dpl_insert_at_index(list, character3, 3, false);
+    dpl_insert_at_index(list, character4, 4, false);
+    dpl_insert_at_index(list, character5, 5, true);
+    dpl_insert_at_index(list, character6, 6, false);
+    ck_assert_msg(dpl_get_element_at_index(list, 5) != character5, "Failure: expected result is not D");
+    ck_assert_msg(dpl_get_element_at_index(list, 99) == character6, "Failure: expected result is E");
+    ck_assert_msg(dpl_get_element_at_index(list, -1) == character, "Failure: expected result is F");
+    ck_assert_msg(dpl_get_element_at_index(list, 4) == character4, "Failure: expected result is G");
+    dpl_free(&list, true);
+    
+}
+END_TEST
+
+START_TEST(test_ListGetListWithRemovedElementAtIndex)
+{
+    dplist_t *list = dpl_create(element_copy, element_free, element_compare);
+    dpl_insert_at_index(list, character, 0, false);
+    dpl_insert_at_index(list, character1, 1, false);
+    dpl_insert_at_index(list, character2, 2, false);
+    dpl_insert_at_index(list, character3, 3, false);
+    dpl_insert_at_index(list, character4, 4, false);
+    dpl_insert_at_index(list, character5, 5, false);
+    dpl_insert_at_index(list, character6, 6, false);
+    dpl_remove_at_index(list, 5, true);
+    ck_assert_msg(dpl_get_element_at_index(list, 5) == character6, "Failure: expected result is E");
+    dpl_remove_at_index(list, 99, true);
+    ck_assert_msg(dpl_get_element_at_index(list, 99) == character4, "Failure: expected result is G");
+    dpl_remove_at_index(list, -1, true);
+    ck_assert_msg(dpl_get_element_at_index(list, -1) == character1, "Failure: expected result is A");
+    dpl_free(&list, true);
+    
+}
+END_TEST
+
+START_TEST(test_ListGetIndexOfElement)
+{
+    dplist_t *list = dpl_create(element_copy, element_free, element_compare);
+    dpl_insert_at_index(list, character, 0, false);
+    dpl_insert_at_index(list, character1, 1, false);
+    dpl_insert_at_index(list, character2, 2, false);
+    dpl_insert_at_index(list, character3, 3, false);
+    dpl_insert_at_index(list, character4, 4, false);
+    dpl_insert_at_index(list, character5, 5, false);
+    dpl_insert_at_index(list, character6, 6, false);
+    my_element_t* k;
+    k->id = 7;
+    k->name = "K";
+    ck_assert_msg(dpl_get_index_of_element(list, character1) == 1, "Failure: expected result is 1");
+    ck_assert_msg(dpl_get_index_of_element(list, character6) == 6, "Failure: expected result is 6");
+    ck_assert_msg(dpl_get_index_of_element(list, character4) == 4, "Failure: expected result is 4");
+    ck_assert_msg(dpl_get_index_of_element(list, k) == -1, "Failure: expected result is -1");
+    dpl_free(&list, true);
+    
+}
+END_TEST
 
 int main(void) {
     Suite *s1 = suite_create("LIST_EX3");
@@ -84,7 +220,11 @@ int main(void) {
     suite_add_tcase(s1, tc1_1);
     tcase_add_checked_fixture(tc1_1, setup, teardown);
     tcase_add_test(tc1_1, test_ListFree);
-    // Add other tests here...
+    tcase_add_test(tc1_1, test_ListInsertAtIndexListNULL);
+    tcase_add_test(tc1_1, test_ListInsertAtIndexListEmpty);
+    tcase_add_test(tc1_1, test_ListGetElementAtIndex);
+    tcase_add_test(tc1_1, test_ListGetListWithRemovedElementAtIndex);
+    tcase_add_test(tc1_1, test_ListGetIndexOfElement);
 
     srunner_run_all(sr, CK_VERBOSE);
 
